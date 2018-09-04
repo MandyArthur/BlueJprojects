7a278d87c79a1076b359ce25a8574d35981f6d18
MandyArthur <Daviddcg8@kent.ac.uk>
2018-09-04 10:24:23 +0100

    Adding Random Behaviour Ex 6.14

---
+++ projects/chapter06/tech-support1/RandomTester.java
@@ -0,0 +1,50 @@
+import java.util.Random; 
+import java.util.*; 
+/**
+ * Write a description of class RandomTester here.
+ * 
+ * @author Mandy Arthur
+ * @version 2018
+ */
+public class RandomTester
+{
+    // instance variables - replace the example below with your own
+    private int printOneRandom;
+    
+    private int howMany; 
+    
+ 
+    /**
+     * Prints one random number 
+     * Reference: https://www.thoughtco.com/how-to-generate-random-numbers-2034206
+     */
+    public int printOneRandom()
+    {
+        
+        Random Generator = new Random(); //Creating a Random object
+        int index = Generator.nextInt(); //nextInt pick any number less than whatever is in the (), in tis case nothing
+        return index; 
+    }
+    
+    /**
+     * Prints multiple random numbers 
+     */
+    
+    public void multiRandNumbers(int howMany)
+    {
+        Random MultiNumbers = new Random(); // Create Random Object 
+        int c; // intialize the amount
+        
+        if (howMany >0){ // if how many is greater than zero 
+            for (c = 1; c <= howMany; c++) {
+      System.out.println("Your #" + c+ " random number: "+ MultiNumbers.nextInt()); // print this 
+    }
+        
+}else{ // if not
+    System.out.println("You asked for zero random numbers.Why?"); // print this instead of spewing error
+        
+    }
+    
+}
+}
+

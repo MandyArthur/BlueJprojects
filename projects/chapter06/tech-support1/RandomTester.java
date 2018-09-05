0be4b1e0e12405652a6c4d5a9f16189068be7f2d
MandyArthur <Daviddcg8@kent.ac.uk>
2018-09-05 09:24:44 +0100

    Ex 6.19 - 6.20

--- projects/chapter06/tech-support1/RandomTester.java
+++ projects/chapter06/tech-support1/RandomTester.java
@@ -66,24 +66,48 @@ public class RandomTester
     }
     
     /**
+     * Ex 6.19 Takes a parameter (max) and generates a random number in the range 1 to max (inclusive)
+     */
+    public void setMaxRandom(int max)
+    {
+        Random maxRandom = new Random(); //Creating a Random object     
+        int index = maxRandom.nextInt(max)+1 ; //simply add 1 to the result of the nextInt() method, to pick a number btn 1-max inclusively add one to the result
+        System.out.println("random number with a maximum of" + max + " : "+ index); //SYS.OUT for void methods 
+        
+    }
+    /**
+     * Ex 6.20 Random within a range (min - max) 
+     * https://www.mkyong.com/java/java-generate-random-integers-in-a-range/
+     */
+    
+    public int randomInRange(int min, int max)
+    {
+        Random randomInRange = new Random();
+		return randomInRange.nextInt((max - min) + 1) + min; 
+		// max - min sets the range (max amount of possible answers
+		// +1 to include the max value 
+		// + min to ensure that the lowest range (0) + min gives you the min range you had set 
+    }
+    
+    /**
      * Prints multiple random numbers 
      */
     
     public void multiRandNumbers(int howMany)
     {
-        Random MultiNumbers = new Random(); // Create Random Object 
-        int c; // intialize the amount
-        
-        if (howMany >0){ // if how many is greater than zero 
-            for (c = 1; c <= howMany; c++) {
-      System.out.println("Your #" + c+ " random number: "+ MultiNumbers.nextInt(10)); // print this 
-    }
-        
-}else{ // if not
-    System.out.println("You asked for zero random numbers.Why?"); // print this instead of spewing error
-        
-    }
+                Random MultiNumbers = new Random(); // Create Random Object 
+                int c; // intialize the amount
+                
+                if (howMany >0){ // if how many is greater than zero 
+                    for (c = 1; c <= howMany; c++) {
+              System.out.println("Your #" + c+ " random number: "+ MultiNumbers.nextInt(10)); // print this 
+            }
+                
+        }else{ // if not
+            System.out.println("You asked for zero random numbers.Why?"); // print this instead of spewing error
+                
+            }
     
-}
+        }
 }
 
